b2d037251523fa9f49d06838c776fb8be681a5a1
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-09-10 19:56:35 -0400

    added new tables for project

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
 module.exports = (sequelize) => {
   const User = require('./user')(sequelize);
   const Profile = require('./profile')(sequelize);
+  const PartnershipRatings = require('./partnershipratings')(sequelize);
 
   User.hasOne(Profile);
   Profile.belongsTo(User);
 
-  return {User, Profile};
+  return {User, Profile, PartnershipRatings};
 };
