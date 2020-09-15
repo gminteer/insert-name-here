d9c7c1db94e16e6e92ea37228e3fcc67dfac2356
smardone <shaun.mardones@gmail.com>
2020-09-15 19:52:09 -0400

    created Messages model associations

--- models/messages.js
+++ models/messages.js
@@ -1,35 +1,17 @@
 const { Model, Datatypes } = require('sequelize');
-const partnership = require('./partnership');
 
 class Messages extends Model {}
 
 Messages.init({
-    body: {
-        type: Datatypes.VARCHAR(500),
-        allowNull: false,
-    },
-    conversation_id: {
-        type: Datatypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'partnership',
-            key: 'id'
+        body: {
+            type: Datatypes.VARCHAR(500),
+            allowNull: false,
         }
     },
-    author_id: {
-        type: Datatypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
+    {
+        sequelize,
+        timestamps: true,
+        underscored: true,
+        modelName: 'messages'
     }
-
-},
-{
-    sequelize,
-    timestamps: true,
-    underscored: true,
-    modelName: 'messages'
-}
 )
